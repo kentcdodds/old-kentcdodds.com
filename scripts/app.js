b75f9429ad92a6f27f1144eb8f0999e5b41da64a
Kent Dodds <kent@doddsfamily.us>
2013-07-27 14:10:09 -0500

    Utilizing HTML5 capabilities for cache manifest. People can see my website even when they're not online! How cool is that!?

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,18 @@
   app.config(function($routeProvider, $locationProvider) {
     $routeProvider.otherwise({redirectTo: '/'});
   });
+  
+  app.run(function($window, $rootScope) {
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+      $rootScope.$apply(function() {
+        $rootScope.online = false;
+      });
+    }, false);
+    $window.addEventListener("online", function () {
+      $rootScope.$apply(function() {
+        $rootScope.online = true;
+      });
+    }, false);
+  });
 })();
