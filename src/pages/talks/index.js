e79d8bc1d03dcd7ece27a658ec8b5c6e55210f6b
Kent C. Dodds <kent@doddsfamily.us>
2017-04-04 12:06:58 -0600

    use glamorous

--- src/pages/talks/index.js
+++ src/pages/talks/index.js
@@ -1,31 +1,26 @@
-import {style} from 'glamor'
+import glamorous from 'glamorous'
 import Presentation from '../../components/presentation'
 import talks from './talk-data'
-
-const styles = {
-  talksRoot: style({textAlign: 'center'}),
-  talksHeading: style({fontSize: 50, marginBottom: 20}),
-  searchPre: style({display: 'inline'}),
-  talksContainer: style({
-    maxWidth: 600,
-    margin: 'auto',
-    textAlign: 'left',
-    fontSize: 18,
-  }),
-}
 
 export default Talks
 
 function Talks() {
   return (
-    <div {...styles.talksRoot}>
-      <h1 {...styles.talksHeading}>
+    <glamorous.Div textAlign="center">
+      <glamorous.H1 fontSize={50} marginBottom={20}>
         Talks
-      </h1>
-      <em>Search with <pre {...styles.searchPre}>⌘/ctrl + f</pre></em>
-      <div {...styles.talksContainer}>
+      </glamorous.H1>
+      <em>
+        Search with <glamorous.Pre display="inline">⌘/ctrl + f</glamorous.Pre>
+      </em>
+      <glamorous.Div
+        maxWidth={600}
+        margin="auto"
+        textAlign="left"
+        fontSize={18}
+      >
         {talks.map((t, i) => <Presentation key={i} {...t} />)}
-      </div>
-    </div>
+      </glamorous.Div>
+    </glamorous.Div>
   )
 }
