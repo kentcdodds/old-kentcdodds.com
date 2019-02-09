adca1de5417d2d5130eabeeca1f88427440f522d
Kent C. Dodds <kent@doddsfamily.us>
2019-02-09 11:26:54 -0700

    add writing page

--- config/website.js
+++ config/website.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
   siteTitle: 'Kent C. Dodds', // Navigation and Site Title
   siteTitleAlt: 'The personal website of Kent C. Dodds', // Alternative Site title for SEO
-  siteTitleShort: 'Kent C. Dodds', // short_name for manifest
+  siteTitleShort: 'kentcdodds', // short_name for manifest
   siteUrl: 'https://kentcdodds.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
