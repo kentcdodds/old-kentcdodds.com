8fd1e549ab4cf8b74782bfe6386caa546eeea716
Kent C. Dodds <kent@doddsfamily.us>
2019-02-18 13:38:28 -0700

    add compound components article

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -83,9 +83,6 @@ export const globalStyles = css`
     font-size: 16px;
     padding: 10px;
     overflow-x: auto;
-    * {
-      white-space: nowrap;
-    }
     /* Track */
     ::-webkit-scrollbar {
       width: 100%;
