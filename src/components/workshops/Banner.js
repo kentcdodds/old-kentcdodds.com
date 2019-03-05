d42efed8320a0c9bf79b0a49b51bc0e57b490499
Vojta Holik <vojta@egghead.io>
2019-03-05 13:38:48 +0100

    tweaks

--- src/components/workshops/Banner.js
+++ src/components/workshops/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {css} from '@emotion/core'
-import {fonts} from '../../lib/typography'
+import {fonts, rhythm} from '../../lib/typography'
+import {bpMaxSM} from '../../lib/breakpoints'
 import Calendar from '../../images/icons/calendar.svg'
 
 const Banner = ({children, title, date, image}) => {
@@ -10,9 +11,23 @@ const Banner = ({children, title, date, image}) => {
         h1 {
           font-family: ${fonts.regular}, sans-serif;
           font-weight: normal;
+          margin-top: 10px;
+        }
+        h4 {
+          font-size: 15px;
+          text-transform: uppercase;
+          letter-spacing: 1px;
+          margin: 0;
+          opacity: 0.8;
         }
         img {
           margin: 0;
+          width: 100%;
+          max-width: 280px;
+          height: auto;
+          ${bpMaxSM} {
+            max-width: 200px;
+          }
         }
         time {
           margin-left: 22px;
@@ -30,6 +45,11 @@ const Banner = ({children, title, date, image}) => {
         ${image &&
           `display: grid;
             grid-template-columns: 1fr 1fr;
+            ${bpMaxSM} {
+                display: flex;
+                flex-direction: column-reverse;
+                align-items: center;
+            }
       `};
       `}
     >
@@ -39,13 +59,31 @@ const Banner = ({children, title, date, image}) => {
           flex-direction: column;
           align-items: flex-start;
           justify-content: center;
+          ${bpMaxSM} {
+            margin-top: ${rhythm(2)};
+            width: 100%;
+          }
         `}
       >
+        <h4>Online Workshop</h4>
         {title && <h1>{title}</h1>}
         <time>{date}</time>
         {children}
       </div>
-      {image && <img src={image} alt={title} />}
+      {image && (
+        <div
+          css={css`
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            ${bpMaxSM} {
+              justify-content: center;
+            }
+          `}
+        >
+          <img src={image} alt={title} />
+        </div>
+      )}
     </div>
   )
 }
