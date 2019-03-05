2dbf3ec8d84030a54218833c8ee7e7589516c51c
Vojta Holik <vojta@egghead.io>
2019-03-05 16:07:10 +0100

    tweaks

--- src/components/workshops/Banner.js
+++ src/components/workshops/Banner.js
@@ -2,16 +2,28 @@ import React from 'react'
 import {css} from '@emotion/core'
 import {fonts, rhythm} from '../../lib/typography'
 import {bpMaxSM} from '../../lib/breakpoints'
-import Calendar from '../../images/icons/calendar.svg'
 import theme from '../../../config/theme'
 import {lighten} from 'polished'
+import CalendarIcon from '../../images/icons/calendar.svg'
+import LocationIcon from '../../images/icons/location.svg'
+import {DaysLeft} from './CallToAction'
 
-const Banner = ({children, title, date, image}) => {
+const Banner = ({children, title, date, location, image}) => {
   return (
     <div
       css={css`
+        background: white;
+        border-radius: 5px;
+        padding: 40px;
+        margin-left: -40px;
+        margin-right: -40px;
+        ${bpMaxSM} {
+          padding: 20px;
+          margin-left: 0;
+          margin-right: 0;
+        }
         h1 {
-          font-family: ${fonts.regular}, sans-serif;
+          font-family: ${fonts.semibold}, sans-serif;
           font-weight: normal;
           margin-top: 10px;
         }
@@ -39,14 +51,28 @@ const Banner = ({children, title, date, image}) => {
             height: 16px;
             position: absolute;
             background-size: 16px 16px;
-            background: url(${Calendar}) no-repeat 0 0 transparent;
+            background: url(${CalendarIcon}) no-repeat 0 0 transparent;
             margin-left: -22px;
+            margin-top: 5px;
+          }
+        }
+        address {
+          margin-left: ${date ? '40px' : '22px'};
+          font-style: normal;
+          ::before {
+            content: '';
+            width: 13px;
+            height: 17px;
+            position: absolute;
+            background-size: 13px 17px;
+            background: url(${LocationIcon}) no-repeat 0 0 transparent;
+            margin-left: -20px;
             margin-top: 5px;
           }
         }
         ${image &&
           `display: grid;
-            grid-template-columns: 1fr 1fr;
+            grid-template-columns: 2fr 1fr;
             ${bpMaxSM} {
                 display: flex;
                 flex-direction: column-reverse;
@@ -54,7 +80,7 @@ const Banner = ({children, title, date, image}) => {
             }
       `};
         .button {
-          margin-top: ${rhythm(1.5)};
+          margin-top: ${rhythm(0.5)};
           padding: 10px 15px;
           background: ${lighten(0.4, `${theme.brand.primary}`)};
           border: 1px solid transparent;
@@ -74,16 +100,33 @@ const Banner = ({children, title, date, image}) => {
           flex-direction: column;
           align-items: flex-start;
           justify-content: center;
+          padding-right: 10px;
+
           ${bpMaxSM} {
             margin-top: ${rhythm(2)};
             width: 100%;
+            padding-right: 0;
           }
         `}
       >
         <h4>Online Workshop</h4>
         {title && <h1>{title}</h1>}
-        <time>{date}</time>
+        <div
+          css={css`
+            display: flex;
+            opacity: 0.8;
+            transition: ${theme.transition.ease};
+            :hover {
+              opacity: 1;
+              transition: ${theme.transition.ease};
+            }
+          `}
+        >
+          {date && <time>{date}</time>}
+          {location && <address>{location}</address>}
+        </div>
         {children}
+        <DaysLeft />
         <a
           href="#register"
           className="button"
@@ -98,8 +141,10 @@ const Banner = ({children, title, date, image}) => {
             display: flex;
             justify-content: flex-end;
             align-items: center;
+            padding-left: 20px;
             ${bpMaxSM} {
               justify-content: center;
+              padding-left: 0;
             }
           `}
         >
--- src/components/workshops/CallToAction.js
+++ src/components/workshops/CallToAction.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import {css} from '@emotion/core'
 import theme from '../../../config/theme'
-import {rhythm} from '../../lib/typography'
+import {rhythm, fonts} from '../../lib/typography'
 import Link from '../Link'
 import {bpMaxSM} from '../../lib/breakpoints'
 import Countdown from 'react-countdown-now'
 
-const Counter = () => (
+const SpotsLeft = 20
+
+export const DaysLeft = () => (
   <Countdown
-    date={1553986740000}
+    date={1553036399000} //3/19/2019 23:59:59
+    renderer={({days}) => {
+      return (
+        <div>
+          {days > 0 ? (
+            <p>
+              The early bird discount of <strong>$200</strong> ends in{' '}
+              <strong>{days}</strong> days!
+            </p>
+          ) : (
+            ''
+          )}
+        </div>
+      )
+    }}
+  />
+)
+
+export const Counter = () => (
+  <Countdown
+    date={1553036399000} //3/19/2019 23:59:59
     renderer={({days, hours, minutes, seconds, completed}) => {
       return (
         <div>
@@ -16,9 +38,10 @@ const Counter = () => (
             <div
               css={css`
                 display: flex;
+                text-align: center;
                 flex-wrap: nowrap;
                 align-items: center;
-                margin-top: ${rhythm(1)};
+                //margin-top: ${rhythm(1)};
                 font-variant-numeric: tabular-nums;
                 font-size: 32px;
                 span:not(:last-of-type) {
@@ -68,55 +91,115 @@ const CallToAction = props => {
       css={css`
         width: 100%;
         margin: 0 auto;
-        display: flex;
-
-        align-items: center;
-        text-align: center;
-        flex-direction: column;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        border-radius: 5px;
+        margin-top: ${rhythm(2.5)};
         background-image: linear-gradient(-213deg, #5e31dc 0%, #3155dc 100%),
           linear-gradient(
             32deg,
             rgba(255, 255, 255, 0.25) 33%,
             rgba(0, 0, 0, 0.25) 100%
           );
+        ${bpMaxSM} {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+        }
         h1,
+        h2,
         h4,
+        h3,
         p,
         div {
           color: ${theme.colors.white};
         }
         h1 {
           max-width: ${rhythm(13)};
-        }
-        border-radius: 5px;
-        margin-top: ${rhythm(2.5)};
-        padding: ${rhythm(2)};
-        ${bpMaxSM} {
-          padding: ${rhythm(1)};
-        }
-        .button {
           margin-bottom: ${rhythm(1)};
+          margin-top: 0;
+        }
+        h2 {
+          font-size: 48px;
+        }
+
+        .button {
+          width: 100%;
           padding: 20px 25px;
-          background: black;
+          background: white;
+          text-align: center;
           border: 1px solid transparent;
-          color: white;
+          color: ${theme.colors.body_color};
           border-radius: 5px;
           :hover {
             border: 1px solid transparent;
             background: ${theme.colors.green_lighten};
           }
         }
+        del {
+          text-transform: line-through;
+          font-size: 50%;
+          opacity: 0.8;
+          font-family: ${fonts.regular};
+        }
       `}
       {...restProps}
     >
-      {title && <h1>{title}</h1>}
-      <h4>Your chance to save $70 in early-bird sale expires in:</h4>
-      <Counter />
-      <p>{props.children}</p>
-
-      <Link to={link} className="button">
-        {buttonText}
-      </Link>
+      <div
+        css={css`
+          width: 100%;
+          display: flex;
+          align-items: flex-start;
+          text-align: left;
+          flex-direction: column;
+          padding: ${rhythm(2)};
+          //box-shadow: inset -10 0 30px hsla(0, 0%, 0%, 0.05);
+          ${bpMaxSM} {
+            padding: ${rhythm(1)};
+            align-items: center;
+            text-align: center;
+          }
+          background: hsla(0, 0%, 0%, 0.2);
+        `}
+      >
+        {title && <h1>{title}</h1>}
+        <p>
+          Your chance to save <strong>$200</strong> in early bird discount ends
+          in:
+        </p>
+        <Counter />
+      </div>
+      <div
+        css={css`
+          display: flex;
+          align-items: flex-start;
+          flex-direction: column;
+          justify-content: space-between;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1)};
+          ${bpMaxSM} {
+            h2 {
+              margin-top: 0;
+            }
+            padding: ${rhythm(1)};
+            justify-content: flex-start;
+            text-align: center;
+            align-items: center;
+          }
+        `}
+      >
+        <>
+          <h2>
+            $300 <del>$500</del>
+          </h2>
+          <p>Spots left: {SpotsLeft}</p>
+          <p>{props.children}</p>
+        </>
+        <Link to={link} className="button">
+          {buttonText}
+        </Link>
+      </div>
     </div>
   )
 }
