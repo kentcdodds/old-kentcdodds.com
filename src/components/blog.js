f8d163f3e6dff46379ca45f3cb5ec5754bbf6a68
Vojta Holik <vojta@egghead.io>
2019-03-19 17:07:19 +0100

    use fluid image

--- src/components/blog.js
+++ src/components/blog.js
@@ -81,7 +81,7 @@ const Blog = ({data: {allMdx}, pageContext: {pagination}, subscribeForm}) => {
                   aria-label={`View ${post.frontmatter.title} article`}
                   to={post.fields.slug}
                 >
-                  <Img fixed={post.frontmatter.banner.childImageSharp.fixed} />
+                  <Img fluid={post.frontmatter.banner.childImageSharp.fluid} />
                 </Link>
               </div>
             )}
