dec4b8532c7ed46458b1106a108ee328d521708a
Vojta Holik <vojta@egghead.io>
2019-03-19 18:43:49 +0100

    bump font weight

--- src/components/mdx/title.js
+++ src/components/mdx/title.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import {fonts} from '../../lib/typography'
 
 export default styled.h1`
   line-height: 1.35;
@@ -6,4 +7,5 @@ export default styled.h1`
   margin-bottom: 20px;
   padding-top: 30px;
   padding-bottom: 10px;
+  font-family: ${fonts.bold}, sans-serif;
 `
