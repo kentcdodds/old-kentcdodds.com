85122b264855b4037cae8555d62a4914a3cf692e
Kent C. Dodds <kent@doddsfamily.us>
2019-03-22 17:39:20 -0600

    fix discuss link

--- src/templates/post.js
+++ src/templates/post.js
@@ -124,7 +124,9 @@ export default function Post({data: {site, mdx}}) {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href={`https://twitter.com/search?q=${encodeURIComponent(
+            // using mobile.twitter.com because if people haven't upgraded
+            // to the new experience, the regular URL wont work for them
+            href={`https://mobile.twitter.com/search?q=${encodeURIComponent(
               blogPostUrl,
             )}`}
           >
