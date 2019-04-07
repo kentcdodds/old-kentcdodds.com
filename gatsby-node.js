6246d7da3328ede4d023da31db07313d95e3a2ce
Kent C. Dodds <kent@doddsfamily.us>
2019-04-07 07:15:39 -0600

    add aha testing blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -187,9 +187,6 @@ exports.onCreateWebpackConfig = ({actions}) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      alias: {
-        $components: path.resolve(__dirname, 'src/components'),
-      },
     },
   })
 }
