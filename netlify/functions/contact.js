658930c52c12633c217b8e013670a3bbdbe4b463
Kent C. Dodds <kent@doddsfamily.us>
2019-04-29 15:42:30 -0600

    add logging to contact form

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -32,6 +32,12 @@ const headers = {
 }
 
 exports.handler = async event => {
+  const runId = Date.now()
+    .toString()
+    .slice(-5)
+  // eslint-disable-next-line no-console
+  const log = (...args) => console.log(runId, ...args)
+
   const origin = new URL(event.headers.origin)
   const acceptable =
     (origin.hostname === 'localhost' &&
@@ -51,10 +57,10 @@ exports.handler = async event => {
       headers,
     }
   }
-  await transporter.verify()
   const {name, email, subject, body, ...otherData} = JSON.parse(event.body)
 
   try {
+    log('> Validating input', ' name: ', name, ' email:', email)
     ow(name, 'Name is too short', ow.string.minLength(1))
     ow(name, 'Name is too long', ow.string.maxLength(60))
     ow(email, 'Email is invalid', isEmail)
@@ -69,6 +75,7 @@ exports.handler = async event => {
       ow.any(ow.string.minLength(40), ow.string.maxLength(1001)),
     )
   } catch (e) {
+    log('> Validation failed', e.message)
     return Promise.reject({
       statusCode: 403,
       body: e.message,
@@ -91,8 +98,12 @@ exports.handler = async event => {
   }
 
   try {
+    log('> Sending...')
+    await transporter.verify()
     await transporter.sendMail(message)
+    log('> Send success!')
   } catch (error) {
+    log('> Send failure!', error.message)
     return Promise.reject({
       statusCode: 500,
       body: error.message,
