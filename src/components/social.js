bfa97bc9640ae89ebf4944b2bed45ecac44f7842
Nicholas Murray <murray.nicholas@gmail.com>
2019-09-09 19:09:34 +0100

    feature: added RSS link and icon to footer (#281)

--- src/components/social.js
+++ src/components/social.js
@@ -120,3 +120,32 @@ export const YouTube = ({
     </svg>
   </Link>
 )
+
+export const RSS = ({
+  color = `rgba(255,255,255,0.7)`,
+  url = `${config.rss}`,
+}) => (
+  <Link
+    to={url}
+    css={css`
+      margin-left: 20px;
+      color: ${color};
+      :hover {
+        color: rgba(255, 255, 255, 1);
+      }
+    `}
+    aria-label="Subscribe to my RSS Feed"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="23"
+      viewBox="0 0 8 8"
+    >
+      <rect width="8" height="8" rx="1.5" fill="currentColor" />
+      <circle cx="2" cy="6" r="1" />
+      <path d="m 1,4 a 3,3 0 0 1 3,3 h 1 a 4,4 0 0 0 -4,-4 z" />
+      <path d="m 1,2 a 5,5 0 0 1 5,5 h 1 a 6,6 0 0 0 -6,-6 z" />
+    </svg>
+  </Link>
+)
