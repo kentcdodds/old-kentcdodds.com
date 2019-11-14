fc9197da39c6f455440a707cd7557736a6f3eec8
Vojta Holik <vojta@egghead.io>
2019-11-14 20:24:00 +0100

    seasons index (#334)

--- src/pages/chats-with-kent-podcast/index.js
+++ src/pages/chats-with-kent-podcast/index.js
@@ -1,26 +1,87 @@
-import React from 'react'
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
 import {graphql} from 'gatsby'
-import {Redirect} from '@reach/router'
-import first from 'lodash/first'
-import get from 'lodash/get'
+import Container from 'components/container'
+import Layout from 'components/layout'
+import PodcastSeason from 'components/podcast/season'
+import DistributionPlatforms from 'components/podcast/platforms'
+import Hero from 'components/big-hero'
+import HeaderImage from '../../images/chats-with-kent.svg'
+import theme from '../../../config/theme'
+import {bpMaxSM, bpMaxMD} from 'lib/breakpoints'
 
-function SeasonIndex({data: {allMdx}}) {
-  const episode = first(allMdx.nodes)
-  return <Redirect noThrow to={`/${get(episode, 'fields.slug', '/')}`} />
+function SeasonIndex({data: {s1}}) {
+  return (
+    <Layout
+      hero={
+        <Hero
+          title="Chats with Kent"
+          text="Kent C. Dodds chats with developers about life, career, and code."
+          image={HeaderImage}
+          background="linear-gradient(213deg, #854BF1 0%, #4335DF 100%), linear-gradient(32deg, rgba(255,255,255,0.25) 33%, rgba(153,153,153,0.25) 100%)"
+        >
+          <div
+            css={{
+              display: 'flex',
+              a: {
+                backgroundColor: '#765CF9',
+                color: 'white !important',
+                border: 'none',
+              },
+              [bpMaxMD]: {
+                paddingBottom: '2rem',
+              },
+              [bpMaxSM]: {
+                paddingBottom: '1rem',
+              },
+            }}
+          >
+            <DistributionPlatforms />
+          </div>
+        </Hero>
+      }
+      headerColor={theme.colors.white}
+    >
+      <Container
+        css={{
+          [bpMaxMD]: {margin: 0},
+          marginTop: -60,
+          position: 'relative',
+          zIndex: 5,
+        }}
+      >
+        <PodcastSeason data={s1} />
+      </Container>
+    </Layout>
+  )
 }
 
 export default SeasonIndex
 
 export const seasonIndexQuery = graphql`
   {
-    allMdx(
+    s1: allMdx(
       filter: {frontmatter: {season: {eq: 1}}, fields: {isPodcast: {eq: true}}}
       sort: {order: ASC, fields: frontmatter___number}
     ) {
+      totalCount
       nodes {
+        id
         fields {
           isPodcast
           slug
+        }
+        frontmatter {
+          title
+          season
+          number
+          guestPhoto {
+            childImageSharp {
+              fluid(maxWidth: 80) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
         }
       }
     }
