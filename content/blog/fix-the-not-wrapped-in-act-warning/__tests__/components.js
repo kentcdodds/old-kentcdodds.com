2a8a68976ea0e8db34a8198c3fe3a448015d6f8e
Kent C. Dodds <me+github@kentcdodds.com>
2020-02-03 16:04:25 -0700

    some updates for act post

--- content/blog/fix-the-not-wrapped-in-act-warning/__tests__/components.js
+++ content/blog/fix-the-not-wrapped-in-act-warning/__tests__/components.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import user from '@testing-library/user-event'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+  act,
+} from '@testing-library/react'
 import {UsernameForm} from '../components'
 
 jest.mock('../api')
@@ -16,22 +21,43 @@ afterEach(() => {
 test('calls updateUsername with the new username (with act warning)', () => {
   const handleUpdateUsername = jest.fn()
   const fakeUsername = 'sonicthehedgehog'
+
   render(<UsernameForm updateUsername={handleUpdateUsername} />)
+
   const usernameInput = screen.getByLabelText(/username/i)
   user.type(usernameInput, fakeUsername)
   user.click(screen.getByText(/submit/i))
+
   expect(handleUpdateUsername).toHaveBeenCalledWith(fakeUsername)
 })
 
 test('calls updateUsername with the new username', async () => {
-  const handleUpdateUsername = jest.fn()
+  const handleUpdateUsername = jest.fn(() => Promise.resolve())
   const fakeUsername = 'sonicthehedgehog'
+
   render(<UsernameForm updateUsername={handleUpdateUsername} />)
+
   const usernameInput = screen.getByLabelText(/username/i)
   user.type(usernameInput, fakeUsername)
   user.click(screen.getByText(/submit/i))
+
   expect(handleUpdateUsername).toHaveBeenCalledWith(fakeUsername)
   await waitForElementToBeRemoved(() => screen.getByText(/saving/i))
+})
+
+test('calls updateUsername with the new username (with manual act and promise)', async () => {
+  const promise = Promise.resolve()
+  const handleUpdateUsername = jest.fn(() => promise)
+  const fakeUsername = 'sonicthehedgehog'
+
+  render(<UsernameForm updateUsername={handleUpdateUsername} />)
+
+  const usernameInput = screen.getByLabelText(/username/i)
+  user.type(usernameInput, fakeUsername)
+  user.click(screen.getByText(/submit/i))
+
+  expect(handleUpdateUsername).toHaveBeenCalledWith(fakeUsername)
+  await act(() => promise)
 })
 
 /*
