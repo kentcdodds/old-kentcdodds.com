5128c7f617231c96e634703ab86df42d95005db8
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-16 14:13:55 -0600

    solidify smoke test

--- cypress/e2e/smoke.js
+++ cypress/e2e/smoke.js
@@ -1,7 +1,9 @@
 describe('app', () => {
   it('works', () => {
     cy.visit('/')
-    cy.findByText(/about/i, {selector: 'a'}).click()
-    cy.findByText(/About Kent C. Dodds/i)
+    cy.findAllByRole('link', {name: /workshops/i})
+      .last()
+      .click()
+    cy.findAllByRole('heading', {name: /Remote Workshops/i})
   })
 })
