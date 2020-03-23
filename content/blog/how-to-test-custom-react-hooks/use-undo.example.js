901c451357d8bc7a20e8e6c1101744565d5d1405
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-23 10:43:09 -0600

    add testing custom react hooks blog post

---
+++ content/blog/how-to-test-custom-react-hooks/use-undo.example.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import useUndo from './use-undo'
+
+function UseUndoExample() {
+  const {present, past, future, set, undo, redo, canUndo, canRedo} = useUndo(
+    'one',
+  )
+  function handleSubmit(event) {
+    event.preventDefault()
+    const input = event.target.elements.newValue
+    set(input.value)
+    input.value = ''
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={undo} disabled={!canUndo}>
+          undo
+        </button>
+        <button onClick={redo} disabled={!canRedo}>
+          redo
+        </button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newValue">New value</label>
+        <input type="text" id="newValue" />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <hr />
+      <div>Present: {present}</div>
+      <div>Past: {past.join(', ')}</div>
+      <div>Future: {future.join(', ')}</div>
+    </div>
+  )
+}
+
+export {UseUndoExample}
