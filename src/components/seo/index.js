a717d5cb3b628ef500835ac5553682ba2bcffb35
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-23 12:32:57 -0600

    update all deps

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import {Helmet} from 'react-helmet'
 import {useStaticQuery, graphql} from 'gatsby'
 import PropTypes from 'prop-types'
 import SchemaOrg from './schema-org'
