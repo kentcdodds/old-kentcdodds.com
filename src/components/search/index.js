452925fa1d3711089a46c6e55bb9df795f792753
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-02 12:36:45 -0600

    fix linting issue

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,8 +1,8 @@
+import React from 'react'
 import styled from '@emotion/styled'
 import {Link as RouterLink} from '@reach/router'
 import Img from 'gatsby-image'
 import {rankings as matchSorterRankings} from 'match-sorter'
-import React from 'react'
 import theme from '../../../config/theme'
 import MatchSorterWorker from './match-sorter.worker'
 
@@ -182,39 +182,39 @@ function Search(props) {
   React.useEffect(() => {
     if (!search) {
       setFilteredBlogPosts(blogposts)
-    } else {
-      getMatchSorterWorker()
-        .searchAndSort(blogposts, search, {
-          keys: [
-            {
-              key: 'title',
-              threshold: matchSorterRankings.CONTAINS,
-            },
-            {
-              key: 'categories',
-              threshold: matchSorterRankings.CONTAINS,
-              maxRanking: matchSorterRankings.CONTAINS,
-            },
-            {
-              key: 'keywords',
-              threshold: matchSorterRankings.CONTAINS,
-              maxRanking: matchSorterRankings.CONTAINS,
-            },
-            {
-              key: 'description',
-              threshold: matchSorterRankings.CONTAINS,
-              maxRanking: matchSorterRankings.CONTAINS,
-            },
-          ],
-        })
-        .then(
-          results => setFilteredBlogPosts(results),
-          error => {
-            // eslint-disable-next-line no-console
-            console.error(error)
-          },
-        )
+      return
     }
+    getMatchSorterWorker()
+      .searchAndSort(blogposts, search, {
+        keys: [
+          {
+            key: 'title',
+            threshold: matchSorterRankings.CONTAINS,
+          },
+          {
+            key: 'categories',
+            threshold: matchSorterRankings.CONTAINS,
+            maxRanking: matchSorterRankings.CONTAINS,
+          },
+          {
+            key: 'keywords',
+            threshold: matchSorterRankings.CONTAINS,
+            maxRanking: matchSorterRankings.CONTAINS,
+          },
+          {
+            key: 'description',
+            threshold: matchSorterRankings.CONTAINS,
+            maxRanking: matchSorterRankings.CONTAINS,
+          },
+        ],
+      })
+      .then(
+        results => setFilteredBlogPosts(results),
+        error => {
+          // eslint-disable-next-line no-console
+          console.error(error)
+        },
+      )
   }, [blogposts, search])
 
   function handleCategoryClick(category) {
